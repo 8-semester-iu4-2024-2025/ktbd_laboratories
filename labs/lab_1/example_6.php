b9be6f84bc7740b5118c403d890ccd871efa811b
kruglovvs <kruglov.valentine@gmail.com>
2025-02-26 22:47:17 +0300

    make example_6 work

--- labs/lab_1/example_6.php
+++ labs/lab_1/example_6.php
@@ -1,9 +1,9 @@
 <?php
-$с = oci_connect("system", "oracle", "//host.docker.internal:1521/xe");
-if ($c) {
-    echo("Successfully connected to Oracle.\n");
-    oci_close($c);
-} else {
+$conn = oci_connect("system", "admin", "host.docker.internal:1521/xe");
+if (! $conn) {
     $err = oci_error();
     echo("Oracle connected Error " . $err[text]);
+} else {
+    echo("Successfully connected to Oracle.\n");
+    oci_close($conn);
 }
