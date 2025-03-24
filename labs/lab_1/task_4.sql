7c537c742538d63e8e32dcd2a79e9a31f5bfb001
kruglovvs <kruglov.valentine@gmail.com>
2025-03-24 23:03:28 +0300

    improve hr user in task_4

--- labs/lab_1/task_4.sql
+++ labs/lab_1/task_4.sql
@@ -1,73 +1,82 @@
 SELECT first_name
       ,last_name
       ,salary
-  FROM employees
- WHERE salary >= 3000
-   AND salary <= 4000
- ORDER BY salary DESC;
+FROM hr.employees
+WHERE salary >= 3000
+      AND salary <= 4000
+ORDER BY salary DESC
+FOR UPDATE NOWAIT;
 SELECT first_name
       ,last_name
       ,manager_id
-  FROM employees
- WHERE manager_id = 105
-    OR manager_id = 149
-    OR manager_id = 205;
+FROM hr.employees
+WHERE manager_id = 105
+      OR manager_id = 149
+      OR manager_id = 205
+FOR UPDATE NOWAIT;
 SELECT last_name
       ,email
       ,phone_number
-  FROM employees
- WHERE instr(
+FROM hr.employees
+WHERE instr(
     email
    ,'H'
    ,2
-) = 2;
+) = 2
+FOR UPDATE NOWAIT;
 SELECT last_name
       ,salary
-      ,comission_pct
-  FROM employees
- WHERE comission_pct = 0
- ORDER BY salary DESC;
+      ,commission_pct
+FROM hr.employees
+WHERE commission_pct = 0
+ORDER BY salary DESC
+FOR UPDATE NOWAIT;
 SELECT first_name
       ,last_name
       ,salary
       ,phone_number
-  FROM employees
- WHERE department_id = 60
-   AND salary >= 3000
- ORDER BY salary DESC;
-SELECT employees.first_name
-      ,employees.last_name
-      ,employees.salary
-      ,employees.job_id
-      ,employees.comission_pct
-  FROM employees
-  LEFT JOIN jobs
-ON employees.job_id = jobs.job_id
- WHERE employees.comission_pct > 0
-    OR instr(
-    jobs.job_title
+FROM hr.employees
+WHERE department_id = 60
+      AND salary >= 3000
+ORDER BY salary DESC
+FOR UPDATE NOWAIT;
+SELECT hr.employees.first_name
+      ,hr.employees.last_name
+      ,hr.employees.salary
+      ,hr.employees.job_id
+      ,hr.employees.commission_pct
+FROM hr.employees
+LEFT JOIN hr.jobs
+ON hr.employees.job_id = hr.jobs.job_id
+WHERE hr.employees.commission_pct > 0
+      OR instr(
+    hr.jobs.job_title
    ,'IT'
    ,1
-) = 1;
+) = 1
+FOR UPDATE NOWAIT;
 SELECT first_name
       ,last_name
       ,manager_id
-  FROM employees
- WHERE manager_id <> 105
-   AND manager_id <> 149
-   AND manager_id <> 205
- ORDER BY last_name;
+FROM hr.employees
+WHERE manager_id <> 105
+      AND manager_id <> 149
+      AND manager_id <> 205
+ORDER BY last_name
+FOR UPDATE NOWAIT;
 SELECT first_name
       ,last_name
       ,hire_date
-  FROM employees
- ORDER BY hire_date ASC;
+FROM hr.employees
+ORDER BY hire_date ASC
+FOR UPDATE NOWAIT;
 SELECT department_id
       ,last_name
       ,phone_number
-  FROM employees
- ORDER BY department_id
-         ,last_name;
+FROM hr.employees
+ORDER BY department_id
+        ,last_name
+FOR UPDATE NOWAIT;
 COLUMN "Подразделение" FORMAT A25
 COLUMN "Подчинение" FORMAT A25
 SELECT lpad(
@@ -79,12 +88,13 @@ SELECT lpad(
     departments.department_name
    ,'/'
 ) "Подчинение"
-  FROM departments
- INNER JOIN employees e1
+FROM hr.departments
+LEFT JOIN hr.employees e1
 ON departments.manager_id = e1.employee_id
- INNER JOIN employees e2
+LEFT JOIN hr.employees e2
 ON e1.manager_id = e2.employee_id
 CONNECT BY NOCYCLE
-    PRIOR departments.department_id = e2.department_id
+    PRIOR e2.department_id = departments.department_id
 START WITH upper(departments.department_name) = 'IT'
- ORDER SIBLINGS BY departments.department_name;
+ORDER SIBLINGS BY departments.department_name
+FOR UPDATE NOWAIT;
