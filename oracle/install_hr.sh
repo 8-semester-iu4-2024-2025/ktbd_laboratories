fcebbd5abed16e65be5e6be940eae4ac333f12dc
kruglovvs <kruglov.valentine@gmail.com>
2025-03-24 23:03:09 +0300

    add another one try to add HR user

---
+++ oracle/install_hr.sh
@@ -0,0 +1,6 @@
+# sqlplus system/oracle_password@localhost:1521 << EOF 
+# @/tmp/db-sample-schemas/human_resources/hr_install.sql hr_password USERS YES \n\r hr_password
+# hr_password \n\r
+# USERS \n\r
+# YES \n\r
+# EOF
